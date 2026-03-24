363ec5c2826a88971067fad79c364173e4c250bf
lottavahe <lotta.vahe@gmail.com>
2026-03-24 17:10:32 +0200

    loin movie entityn, sille konstruktorin ja getterit ja setterit

--- harjoitusprojekti/src/main/java/backend/harjoitusprojekti/model/Movie.java
+++ harjoitusprojekti/src/main/java/backend/harjoitusprojekti/model/Movie.java
@@ -1,5 +1,89 @@
 package backend.harjoitusprojekti.model;
 
+import java.sql.Date;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String director;
+    private int releaseYear;
+    private String genre;
+    private int duration;
+
+    public Movie() {
+
+    }
+
+    public Movie(String name, String director, int releaseYear, String genre, int duration) {
+        super();
+        this.name = name;
+        this.director = director;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+        this.duration = duration;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+    
 
 }
