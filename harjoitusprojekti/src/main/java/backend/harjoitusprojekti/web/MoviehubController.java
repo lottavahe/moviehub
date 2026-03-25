399f754ba760def6b4157355e248891d6bbd8374
lottavahe <lotta.vahe@gmail.com>
2026-03-25 18:08:29 +0200

    lisäsin add ja delete toiminnot srajoille ja elokuville

--- harjoitusprojekti/src/main/java/backend/harjoitusprojekti/web/MoviehubController.java
+++ harjoitusprojekti/src/main/java/backend/harjoitusprojekti/web/MoviehubController.java
@@ -2,12 +2,15 @@ package backend.harjoitusprojekti.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import backend.harjoitusprojekti.model.MovieRepository;
+import backend.harjoitusprojekti.model.Serie;
 import backend.harjoitusprojekti.model.SerieRepository;
 import backend.harjoitusprojekti.model.GenreRepository;
+import backend.harjoitusprojekti.model.Movie;
 
 
 @Controller
@@ -26,6 +29,8 @@ public class MoviehubController {
     public String showMoviehub() {
         return "moviehub";
     }
+
+
 //näyttäää kaikki elokuvat
     @RequestMapping("/movies")
     public String showAllMovies(Model model) {
@@ -53,6 +58,50 @@ public class MoviehubController {
         model.addAttribute("watchedMovies", movieRepository.findByWatchedTrue());
         model.addAttribute("watchedSeries", serieRepository.findByWatchedTrue());
         return "watched";
+
+
+
+    }
+    //poistaa elokuvan (myöh lisää oikeus vain adminille)
+    @RequestMapping(value = "/deletemovie/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable Long id, Model model) {
+        movieRepository.deleteById(id);
+        return "redirect:/movies";
     }
 
+    //lisää elokuvan (myöh lisää oikeus admin)
+    @RequestMapping(value = "/addmovie")
+    public String addMovie(Model model) {
+        model.addAttribute("movie", new Movie());
+        model.addAttribute("genres", genreRepository.findAll());
+        return "addmovie";
+    }
+    //poistaa sarjan (myöh lisää oikeus vain adminille)
+    @RequestMapping(value = "/deleteserie/{id}", method = RequestMethod.GET)
+    public String deleteSerie(@PathVariable Long id, Model model) {
+        serieRepository.deleteById(id);
+        return "redirect:series";
+    }
+    //lisää sarjjan (myöh lisää oikeus admin)
+    @RequestMapping(value = "/addserie")
+    public String addBook(Model model) {
+        model.addAttribute("serie", new Serie());
+        model.addAttribute("genres", genreRepository.findAll());
+        return "addserie";
+    }
+
+
+
+    //tallennna elokuva
+    @RequestMapping(value = "/savemovie", method = RequestMethod.POST)
+    public String savemovie(Movie movie) {
+        movieRepository.save(movie);
+        return "redirect:/movies";
+    }
+    //tallenna sarja
+    @RequestMapping(value = "/saveserie", method = RequestMethod.POST)
+    public String saveserie(Serie serie) {
+        serieRepository.save(serie);
+        return "redirect:/series";
+    }
 }
--- harjoitusprojekti/src/main/java/backend/harjoitusprojekti/web/MoviehubRestController.java
+++ harjoitusprojekti/src/main/java/backend/harjoitusprojekti/web/MoviehubRestController.java
@@ -1,7 +1,5 @@
 package backend.harjoitusprojekti.web;
 
-import java.util.Optional;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
