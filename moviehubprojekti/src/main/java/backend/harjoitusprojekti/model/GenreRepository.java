643d18c368d94319da628ad14fb541b0e0eb10a6
lottavahe <lotta.vahe@gmail.com>
2026-03-25 11:36:47 +0200

    koitan korjata ongelmaa elovien ja sarjojen siirrossa watchlistiin, välitallennus

---
+++ moviehubprojekti/src/main/java/backend/harjoitusprojekti/model/GenreRepository.java
@@ -0,0 +1,8 @@
+package backend.harjoitusprojekti.model;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GenreRepository extends CrudRepository<Genre, Long> {
+    List<Genre> findByGenreName(String genreName);
+}
---
+++ moviehubprojekti/src/main/java/backend/harjoitusprojekti/model/Serie.java
@@ -0,0 +1,129 @@
+package backend.harjoitusprojekti.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String creator;
+    private int releaseYear;
+    private int seasons;
+    private int episodes;
+    private int duration;
+
+    private boolean inWatchlist;
+    private boolean watched;
+    
+    @ManyToOne
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+
+    public Serie() { }
+
+    public Serie(String title, String creator, int releaseYear, int seasons, int episodes, int duration, Genre genre) {
+        super();
+        this.title = title;
+        this.creator = creator;
+        this.releaseYear = releaseYear;
+        this.seasons = seasons;
+        this.episodes = episodes;
+        this.duration = duration;
+        this.genre = genre;
+        this.inWatchlist = false;
+        this.watched = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public int getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(int seasons) {
+        this.seasons = seasons;
+    }
+
+    public int getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+    public boolean isInWatchlist() {
+        return inWatchlist;
+    }
+
+    public void setInWatchlist(boolean inWatchlist) {
+        this.inWatchlist = inWatchlist;
+    }
+
+    public boolean isWatched() {
+        return watched;
+    }
+
+    public void setWatched(boolean watched) {
+        this.watched = watched;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+    
+    
+}
