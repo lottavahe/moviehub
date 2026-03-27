6ac36ccdc16f4074fb368615003f966817390943
lottavahe <lotta.vahe@gmail.com>
2026-03-27 13:58:06 +0200

    sain rekisteröinnin toimimaan, seuraavaksi koitan saada kaikille sivulle järkevät ilmoitukset

--- harjoitusprojekti/src/main/java/backend/harjoitusprojekti/web/RegisterController.java
+++ harjoitusprojekti/src/main/java/backend/harjoitusprojekti/web/RegisterController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import backend.harjoitusprojekti.service.UserService;
 import org.springframework.ui.Model;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class RegisterController {
